54122cedeedd2165042703bbccff78f69d072e26
Costa, C.J <ccosta@estgl.ipv.pt>
2023-01-12 16:26:25 +0000

    CRUD completo

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
+use Illuminate\Support\Facades\File;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
+
     /**
      * Display a listing of the resource.
      *
@@ -14,7 +16,8 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        $data = Product::latest()->paginate(5);
+        return view('products.index', compact('data'))->with('i',(request()->input('page', 1) - 1) * 5);
     }
 
     /**
@@ -35,7 +38,20 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' => 'required',
+            'detail' => 'required',
+            'product_image' => 'required|image|mimes:jpg,png,jpeg,gif,svg|dimensions:min_with=100,
+                    min_height=100,max_with=1000,max_height=1000'
+        ]);
+        $file_name = time() . '.' . request()->product_image->getClientOriginalExtension();
+        request()->product_image->move(public_path('images'), $file_name);
+        $product = new Product;
+        $product->name = $request->name;
+        $product->detail = $request->detail;
+        $product->product_image = $file_name;
+        $product->save();
+        return redirect()->route('products.index')->with('success', 'Produto adicionado com sucesso');
     }
 
     /**
@@ -46,7 +62,7 @@ class ProductController extends Controller
      */
     public function show(Product $product)
     {
-        //
+        return view('products.show', compact('product'));
     }
 
     /**
@@ -57,7 +73,7 @@ class ProductController extends Controller
      */
     public function edit(Product $product)
     {
-        //
+        return view('products.edit', compact('product'));
     }
 
     /**
@@ -69,7 +85,31 @@ class ProductController extends Controller
      */
     public function update(Request $request, Product $product)
     {
-        //
+        $request->validate([
+            'name' => 'required',
+            'detail' => 'required',
+            'product_image' => 'image|mimes:jpg,png,jpeg,gif,svg|dimensions:min_with=100,
+                    min_height=100,max_with=1000,max_height=1000'
+        ]);
+        $product_image = $request->hidden_product_image;
+
+        if($request->product_image != '')
+        {
+            $image_path = public_path().'/images/'.$product_image;
+            if(File::exists($image_path))
+            {
+                File::delete($image_path);
+            }
+            $product_image = time() . '.' . request()->product_image->getClientOriginalExtension();
+            request()->product_image->move(public_path('images'), $product_image);
+        }
+
+        $product = Product::find($request->hidden_id);
+        $product->name = $request->name;
+        $product->detail = $request->detail;
+        $product->product_image = $product_image;
+        $product->save();
+        return redirect()->route('products.index')->with('success', 'Produto atualizado com sucesso');
     }
 
     /**
@@ -80,6 +120,12 @@ class ProductController extends Controller
      */
     public function destroy(Product $product)
     {
-        //
+            $image_path = public_path().'/images/'.$product->product_image;
+            if(File::exists($image_path))
+            {
+                File::delete($image_path);
+            }
+            $product->delete();
+            return redirect()->route('products.index')->with('success', 'Produto eliminado com sucesso');
     }
 }
